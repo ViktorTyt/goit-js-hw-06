ed9a7e1c4981804835f12fd579e12fb7db68fbcc
Viktor Tytenko <92685344+ViktorTyt@users.noreply.github.com>
2022-04-24 00:44:45 +0300

    fixed task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,14 +1,18 @@
 const getRandomHexColor = () => {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 };
 
 const refs = {
   btn: document.querySelector(".change-color"),
   colorText: document.querySelector(".color"),
+  body: document.querySelector("body"),
 };
 
 const handleTargetButtonClick = () => {
   refs.colorText.textContent = getRandomHexColor();
+  refs.body.style = `background-color: ${refs.colorText.textContent}`;
 };
 
 const showRandomHexColor = refs.btn.addEventListener(
--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,18 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
+const refs = {
+  input: document.querySelector("#controls input"),
+  btnCreate: document.querySelector("[data-create]"),
+  btnDestroy: document.querySelector("[data-destroy]"),
+};
+
+console.log(refs.input);
+console.log(refs.btnCreate);
+console.log(refs.btnDestroy);
+
+const handleTragetBtnClick = (event) => {
+  console.log(15);
+  console.log(document.createElement("div") * );
+};
+const createCollection = refs.btnCreate.addEventListener(
+  "click",
+  handleTragetBtnClick
+);
