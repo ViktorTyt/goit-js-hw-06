4e1c2ec91e6af68d83997f75bc2df05f4d7245d0
Viktor Tytenko <92685344+ViktorTyt@users.noreply.github.com>
2022-04-23 19:46:05 +0300

    made task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,24 @@
+const formEl = document.querySelector(".login-form");
 
+const handleFormSubmit = (event) => {
+  event.preventDefault();
+
+  const formElements = event.currentTarget.elements;
+
+  const mail = formElements.email.value;
+  const password = formElements.password.value;
+
+  const formData = { mail: "", password: "" };
+
+  if (mail === "" || password === "") {
+    alert("Всі поля повинні бути заповнені");
+  } else {
+    formData.mail = mail;
+    formData.password = password;
+    console.log(formData);
+  }
+
+  event.currentTarget.reset();
+};
+
+formEl.addEventListener("submit", handleFormSubmit);
